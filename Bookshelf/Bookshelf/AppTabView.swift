529d1b033b4b4725ede907d469e8494db2442c90
Jc-hammond <jasonchammond@gmail.com>
2021-08-22 18:29:44 -0600

    testing adding a file

---
+++ Bookshelf/Bookshelf/AppTabView.swift
@@ -0,0 +1,20 @@
+//
+//  AppTabView.swift
+//  Bookshelf
+//
+//  Created by Connor Hammond on 8/22/21.
+//
+
+import SwiftUI
+
+struct AppTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppTabView()
+    }
+}
